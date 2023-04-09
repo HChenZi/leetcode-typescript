7237ccc7377e4c9ad5e781d7c1a742f9313da671
HChenZi <carbonblackorange@gmail.com>
2023-04-09 23:47:49 +0800

    206-reverse-linked-list

---
+++ src/206-reverse-linked-list.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let first = head ? new ListNode(head?.val, null) : null
+  while(head?.next) {
+    head = head.next
+    first = new ListNode(head?.val, first)
+  }
+  return first
+};
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+console.log(reverseList);
